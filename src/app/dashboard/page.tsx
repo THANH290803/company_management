eb71f2bd675a2dd7afb722c7e652dd6df99a514b
THANH290803 <phucthanh290803@gmail.com>
2025-04-08 16:02:51 +0700

    auto height dasboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -23,7 +23,7 @@ import { LineChart1 } from "@/components/Chart/line-chart"
 export default function Page() {
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar className="h-auto" />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
